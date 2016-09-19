2d0255d25b221276aaabda2f98f43d21e36acb6f
Yakima-Teng <cleveryun@163.com>
2016-09-19 17:33:42 +0800

    添加src/scripts/data目录用于将数据放进多个文件分别单独存放，便于后期维护

---
+++ src/scripts/data/demos.js
@@ -0,0 +1,48 @@
+/***********************************************************************************
+ *                                                                                  *
+ * demos
+ *                                                                                  *
+ ***********************************************************************************/
+const demos = {
+  title: '作品案例',
+  description: '',
+  note: '',
+  items: [
+    {
+      title: 'LetsBlog',
+      skills: ['Node.js', 'MySQL'],
+      duration: '2012.11-Now',
+      description: [
+        '为建立个人博客而开发的博客发布系统（上一版基于ASP.NET+SQLite开发），基于[[Express]]框架开发，页面由[[XTemplate]]模板引擎渲染，目前通过iisnode运行于IIS服务器上。',
+        '前端方面，应用了[[媒体查询]]、[[字体图标]]等技术，还引入了[[highlight.js]]高亮代码，使用户体验更佳。'
+      ],
+      gitUrl: 'https://github.com/heeroluo/letsblog',
+      demoUrl: 'http://heeroluo.net/',
+      imgUrl: 'http://cv.heeroluo.net/images/project-letsblog.jpg'
+    },
+    {
+      title: 'LetsBlog',
+      skills: ['Node.js', 'MySQL'],
+      duration: '2012.11-Now',
+      description: [
+        '为建立个人博客而开发的博客发布系统（上一版基于ASP.NET+SQLite开发），基于[[Express]]框架开发，页面由[[XTemplate]]模板引擎渲染，目前通过iisnode运行于IIS服务器上。',
+        '前端方面，应用了[[媒体查询]]、[[字体图标]]等技术，还引入了[[highlight.js]]高亮代码，使用户体验更佳。'
+      ],
+      gitUrl: 'https://github.com/heeroluo/letsblog',
+      demoUrl: 'http://heeroluo.net/',
+      imgUrl: 'http://cv.heeroluo.net/images/project-letsblog.jpg'
+    },
+    {
+      title: 'LetsBlog',
+      skills: ['Node.js', 'MySQL'],
+      duration: '2012.11-Now',
+      description: [
+        '为建立个人博客而开发的博客发布系统（上一版基于ASP.NET+SQLite开发），基于[[Express]]框架开发，页面由[[XTemplate]]模板引擎渲染，目前通过iisnode运行于IIS服务器上。',
+        '前端方面，应用了[[媒体查询]]、[[字体图标]]等技术，还引入了[[highlight.js]]高亮代码，使用户体验更佳。'
+      ],
+      gitUrl: 'https://github.com/heeroluo/letsblog',
+      demoUrl: 'http://heeroluo.net/',
+      imgUrl: ''
+    }
+  ]
+}
---
+++ src/scripts/data/skills.js
@@ -0,0 +1,19 @@
+/***********************************************************************************
+ *                                                                                  *
+ * sills
+ *                                                                                  *
+ ***********************************************************************************/
+const skills = [
+  {
+    keywords: 'NodeJS, ES6, ReactJS, AngularJS 1+, VueJS 1+',
+    percentage: '60%'
+  },
+  {
+    title: 'CSS, LESS',
+    percentage: '30%'
+  },
+  {
+    title: 'Zhuangbiability',
+    percentage: '10%'
+  }
+]
