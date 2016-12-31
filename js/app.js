0747a1703820ac13ee8e70fe35f20477bfb76e89
Yakima-Teng <cleveryun@163.com>
2016-12-31 19:53:06 +0800

    Updates

--- js/app.js
+++ js/app.js
@@ -20786,23 +20786,58 @@ var demos = {
   items: [{
     title: '车险投保PC端管理系统',
     duration: '2012.11-Now',
-    description: ['bla bla bla bla bla bla', 'bla bla bla bla bla bla bla bla bla bla'],
-    // gitUrl: 'https://github.com/Yakima-Teng/about',
-    demoUrl: 'https://yakima-teng.github.io/about/',
-    imgUrl: ''
-  }, {
-    title: '项目2',
-    duration: '2012.11-Now',
-    description: ['bla bla bla bla bla bla', 'bla bla bla bla bla bla bla bla bla bla'],
+    description: ['未使用框架（jQuery是库不是框架）', '主体内容的切换借助ifame标签实现', '页面间的数据共享借助localStorage实现', '打印'],
     gitUrl: '',
-    demoUrl: 'https://yakima-teng.github.io/about/',
+    demoUrl: '',
     imgUrl: ''
   }, {
-    title: '项目3',
+    title: '车辆估损PC端后台管理系统',
     duration: '2012.11-Now',
-    description: ['bla bla bla bla bla bla', 'bla bla bla bla bla bla bla bla bla bla'],
-    gitUrl: 'https://github.com/Yakima-Teng/about',
-    demoUrl: 'https://yakima-teng.github.io/about/',
+    description: ['Vue1、websocket聊天、图片上传'],
+    gitUrl: '',
+    demoUrl: '',
+    imgUrl: ''
+  }, {
+    title: '车险比价微信端项目',
+    duration: '2012.11-Now',
+    description: ['Vue1、一堆表单、借助rem单位实现自适应布局'],
+    gitUrl: '',
+    demoUrl: '',
+    imgUrl: ''
+  }, {
+    title: '博客',
+    duration: '2012.11-Now',
+    description: ['Angular1，接口由NodeJS+Mysql提供', '支持根据来源显示对应的上一篇文章和下一篇文章', '支持简单的评论功能'],
+    gitUrl: 'https://github.com/Yakima-Teng/blog',
+    demoUrl: 'http://www.orzzone.com/blog/',
+    imgUrl: ''
+  }, {
+    title: 'react-webpack项目脚手架',
+    duration: '2012.11-Now',
+    description: ['react + react-router + webpack + ES6 + babel + eslint', '由Vue官方webpack模版改造而来的react项目开发脚手架，预置了一个可由鼠标操控的六面体'],
+    gitUrl: 'https://github.com/Yakima-Teng/react-webpack-boilerplate',
+    demoUrl: 'http://www.orzzone.com/react-webpack-boilerplate/',
+    imgUrl: ''
+  }, {
+    title: 'vue2-webpack项目脚手架',
+    duration: '2012.11-Now',
+    description: ['vue2 + vue-router + vuex + webpack + ES6 + eslint', '由Vue官方webpack模版改造而来的vue2项目开发脚手架'],
+    gitUrl: 'https://github.com/Yakima-Teng/vue2-webpack-boilerplate',
+    demoUrl: 'https://yakima-teng.github.io/vue2-webpack-boilerplate/',
+    imgUrl: ''
+  }, {
+    title: 'mock server',
+    duration: '2012.11-Now',
+    description: ['服务器端数据模拟，方便前端工程师独立于后端进行开发', '支持代理请求、响应静态JSON文件数据、响应自定义动态数据'],
+    gitUrl: 'https://github.com/Yakima-Teng/mock-server',
+    demoUrl: '',
+    imgUrl: ''
+  }, {
+    title: 'iframe application',
+    duration: '2012.11-Now',
+    description: ['基于iframe实现的类SPA应用脚手架', '适用于有对前端了解较少的后端参与的前端应用类项目的开发'],
+    gitUrl: 'https://github.com/Yakima-Teng/iframe-application',
+    demoUrl: '',
     imgUrl: ''
   }]
 };
