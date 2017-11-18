68477cbef072ec7e4f26578892927f1acfdd9e2d
Yakima <cleveryun@163.com>
2017-11-18 10:48:14 +0800

    Updates

--- js/app.js
+++ js/app.js
@@ -9926,28 +9926,28 @@ var basic = {
   },
   details: [{
     title: {
-      CH: '码云Git',
-      EN: 'Git (OSChina)'
-    },
-    text: 'github.com/Yakima-Teng',
-    destinationUrl: 'https://git.oschina.net/yakima',
-    imgUrl: 'img/coding.gif'
-  }, {
-    title: {
-      CH: '读书（130）',
-      EN: 'Reading (130)'
+      CH: '读书',
+      EN: 'Reading'
     },
     text: 'book.douban.com/people/cleveryun/',
     destinationUrl: 'https://book.douban.com/people/cleveryun/collect?sort=time&start=0&filter=all&mode=list&tags_sort=count',
     imgUrl: 'img/reading.jpg'
   }, {
     title: {
-      CH: '写博8年',
-      EN: '8-year blogger'
+      CH: '写作',
+      EN: 'Writing'
     },
     text: 'orzzone.com',
     destinationUrl: 'http://www.orzzone.com',
     imgUrl: 'img/blogging.jpg'
+  }, {
+    title: {
+      CH: '敲代码',
+      EN: 'Coding'
+    },
+    text: 'github.com/Yakima-Teng',
+    destinationUrl: 'https://github.com/Yakima-Teng',
+    imgUrl: 'img/coding.gif'
   }]
   /***********************************************************************************
    *                                                                                  *
